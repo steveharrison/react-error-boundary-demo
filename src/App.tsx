aedabbb96a7ce3b427ba61c48ff5f3b7764178dc
Steve Harrison <steveharrison.dev@icloud.com>
2022-08-03 20:48:27 +1000

    Added an error boundary example.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import ErrorBoundary from './ErrorBoundary';
 import './App.css';
+
+async function getPrice():Promise<number> {
+  const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+
+  // throw new Error("This won't be caught by the ErrorBoundary.");
+
+  const responseJSON = await response.json();
+
+  return responseJSON.bitcoin.usd;
+}
+
+function Hero() {
+  const [price, setPrice] = useState<number | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const bitcoinPrice = await getPrice();
+      setPrice(bitcoinPrice);
+    })();
+  });
+
+  throw new Error('This will be caught by the ErrorBoundary.');
+
+  return (
+    <>
+      <h2>Bitcoin price:</h2>
+      <div>{price}</div>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ErrorBoundary>
+        <Hero/>
+      </ErrorBoundary>
     </div>
   );
 }
---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
